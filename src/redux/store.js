cf06944bc0719f885e99fcb20feae6bbda1d0956
JRaamos <jhonyramos46@gmail.com>
2023-07-10 18:25:29 -0300

    feat: configura ambiente de desenvolvimento

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+// arquivo em que a redux store é criada
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+if (window.Cypress) {
+  window.store = store;
+}
+
+export default store;
