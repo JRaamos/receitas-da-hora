70fbd46155f5b956f6a6d3dbac33d3d7a672e730
Luiz Octavio <luuizoctavio.ss@outlook.com>
2023-07-11 15:22:35 -0300

    feat: implementa o menu inferior com cada icone redirecionando para paginas especificas. Testes feitos com cobertura de 100%

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Meals() {
   return (
     <div>
-      <Header />
+      <Header title="Meals" />
+      <h1>Meals</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/tests/Footer.spec.js
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// import App from '../App';
+import renderWithRouter from '../helpers/renderWith';
+import Footer from '../components/Footer';
+
+describe('Testando o componente Footer', () => {
+  it('Teste se o componente contém os ícones corretos de acordo com a rota', () => {
+    renderWithRouter(<Footer />);
+    const footer = screen.getByTestId('footer');
+    const icons = screen.getAllByRole('img');
+    expect(footer).toBeInTheDocument();
+    expect(icons.length).toBe(2);
+  });
+  it('Verifica se ao clicar no ícone de bebidas, a rota muda para a tela /drinks', () => {
+    const { history } = renderWithRouter(<Footer />);
+    const drinksIcon = screen.getByTestId('drinks-bottom-btn');
+    userEvent.click(drinksIcon);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/drinks');
+  });
+  it('Verifica se ao clicar no ícone de comidas, a rota muda para a tela /meals', () => {
+    const { history } = renderWithRouter(<Footer />);
+    const mealsIcon = screen.getByTestId('meals-bottom-btn');
+    userEvent.click(mealsIcon);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/meals');
+  });
+});
