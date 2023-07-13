d0ef4a16543ffc16ca8736acfa21f02d51cfdc0a
Luiz Octavio <luuizoctavio.ss@outlook.com>
2023-07-13 16:15:04 -0300

    feat: req 38 e 39: Desenvolve um checkbox para cada item da lista de ingredientes. Implementa uma lógica que ao clicar no checkbox de um ingrediente, o nome dele deve ser "riscado" da lista

--- src/components/RecipeInProgress.js
+++ src/components/RecipeInProgress.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 import { fetchAPIId } from '../helpers/fetchApiId';
+import './recipeInProgress.css';
+
+const getIngredients = (recipe) => Object.entries(recipe)
+  .filter(([key, value]) => key.includes('strIngredient') && value)
+  .map(([, value]) => value);
 
 function RecipeInProgress() {
   const location = useLocation();
@@ -19,6 +24,7 @@ function RecipeInProgress() {
       title: strMeal || strDrink,
       category: strCategory,
       instructions: strInstructions,
+      ingredients: getIngredients(item),
     };
   };
 
@@ -34,11 +40,26 @@ function RecipeInProgress() {
   console.log(recipe);
   return (
     <div>
-      <img data-testid="recipe-photo" src={ recipe.image } alt={ recipe.title } />
-      <h1 data-testid="recipe-title">{recipe.title}</h1>
-      <button type="button" data-testid="share-btn">Share</button>
-      <button type="button" data-testid="favorite-btn">Favorite</button>
-      <h3 data-testid="recipe-category">{recipe.category}</h3>
+      <header>
+        <h1 data-testid="recipe-title">{recipe.title}</h1>
+        <img data-testid="recipe-photo" src={ recipe.image } alt={ recipe.title } />
+        <h3 data-testid="recipe-category">{recipe.category}</h3>
+        <button type="button" data-testid="favorite-btn">Favorite</button>
+        <button type="button" data-testid="share-btn">Share</button>
+      </header>
+      <div className="ingredient-progress">
+        <h3>Ingredients</h3>
+        <ul className="ingredient-progress">
+          {recipe.ingredients && recipe.ingredients.map((ingredient, index) => (
+            <li key={ index } data-testid={ `${index}-ingredient-step` }>
+              <label>
+                <input type="checkbox" className="ingredient-input" />
+                {ingredient}
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
       <p data-testid="instructions">{recipe.instructions}</p>
       <button type="button" data-testid="finish-recipe-btn">Finish Recipe</button>
     </div>
