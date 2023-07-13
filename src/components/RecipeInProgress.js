9178143130c3da05668faf2ab57487fff88d6414
Luiz Octavio <luuizoctavio.ss@outlook.com>
2023-07-13 17:48:20 -0300

    feat: req 40: Salva o estado do progresso, que deve ser mantido caso a pessoa atualize a página ou volte para a mesma receita

--- src/components/RecipeInProgress.js
+++ src/components/RecipeInProgress.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 import { fetchAPIId } from '../helpers/fetchApiId';
 import './recipeInProgress.css';
+import { getLocalStorage } from '../utils/localStorage';
 
 const getIngredients = (recipe) => Object.entries(recipe)
   .filter(([key, value]) => key.includes('strIngredient') && value)
@@ -11,15 +12,19 @@ function RecipeInProgress() {
   const location = useLocation();
   const { pathname } = location;
   const [recipe, setRecipe] = useState({});
+  const [ingredients, setIngredients] = useState([]);
 
   const trataItem = (item) => {
     const { strMealThumb,
       strDrinkThumb,
       strMeal, strDrink,
       strCategory,
-      strInstructions } = item;
+      strInstructions,
+      idDrink,
+      idMeal } = item;
 
     return {
+      id: idMeal || idDrink,
       image: strMealThumb || strDrinkThumb,
       title: strMeal || strDrink,
       category: strCategory,
@@ -29,15 +34,30 @@ function RecipeInProgress() {
   };
 
   useEffect(() => {
+    const id = pathname.split('/')[2];
     const recipeInProgress = async () => {
-      const id = pathname.split('/')[2];
       const type = pathname.split('/')[1];
       const data = await fetchAPIId(id, type);
       setRecipe(trataItem(data));
     };
     recipeInProgress();
+    const catchLocalStorage = getLocalStorage.getItem('inProgressRecipes') || {};
+    setIngredients((catchLocalStorage[id]) || []);
   }, [pathname]);
-  console.log(recipe);
+
+  useEffect(() => {
+    const { id } = recipe;
+    getLocalStorage.setItem('inProgressRecipes', { [id]: ingredients });
+  }, [ingredients, recipe]);
+
+  const handleChangeCheckbox = ({ target }, index) => {
+    const { checked } = target;
+    ingredients[index] = true;
+    setIngredients((state) => {
+      state[index] = checked;
+      return [...state];
+    });
+  };
   return (
     <div>
       <header>
@@ -53,7 +73,12 @@ function RecipeInProgress() {
           {recipe.ingredients && recipe.ingredients.map((ingredient, index) => (
             <li key={ index } data-testid={ `${index}-ingredient-step` }>
               <label>
-                <input type="checkbox" className="ingredient-input" />
+                <input
+                  type="checkbox"
+                  className="ingredient-input"
+                  onChange={ (event) => handleChangeCheckbox(event, index) }
+                  checked={ Boolean(ingredients[index]) }
+                />
                 {ingredient}
               </label>
             </li>
---
+++ src/utils/localStorage.js
@@ -0,0 +1,7 @@
+export const getLocalStorage = {
+  setItem: (key, value) => localStorage.setItem(key.toLowerCase(), JSON.stringify(value)),
+  getItem: (key) => {
+    const result = localStorage.getItem(key.toLowerCase());
+    return result && JSON.parse(result);
+  },
+};
