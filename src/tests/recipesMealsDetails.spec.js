fcbe76512196875ff467cf86c3da6c2da55766ab
JRaamos <jhonyramos46@gmail.com>
2023-07-14 01:28:35 -0300

    test: adiciona testes para a pagina de detalhes de comida

--- src/tests/recipesMealsDetails.spec.js
+++ src/tests/recipesMealsDetails.spec.js
@@ -1,15 +1,37 @@
 import { screen } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from '../helpers/renderWith';
-import RecipesMealsDetails from '../pages/RecipesMealsDetails';
+import App from '../App';
 
 describe('Testa o endpoint de detalhes de uma receita de drinks', () => {
-  it('Testa se é rederizado o componente ', () => {
-    const { history } = renderWithRouterAndRedux(<RecipesMealsDetails />);
+  it('Testa se é rederizado o componente ', async () => {
+    const { history } = renderWithRouterAndRedux(<App />);
     act(() => {
-      history.push('/meals/52771');
+      history.push('/meals');
     });
-    const title = screen.getByText('RecipesMealsDetails');
+    expect(history.location.pathname).toBe('/meals');
+
+    const cardCorba = await screen.findByTestId('0-card-img');
+    act(() => {
+      userEvent.click(cardCorba);
+    });
+    expect(history.location.pathname).toBe('/meals/52977');
+
+    const title = await screen.findByTestId('recipe-title');
+    const img = await screen.findByTestId('recipe-photo');
+    const category = await screen.findByTestId('recipe-category');
+    const instructions = await screen.findByTestId('instructions');
+    const video = await screen.findByTestId('video');
+    const recomendation = await screen.findByTestId('0-recommendation-card');
+    const recomendationTitle = await screen.findByTestId('0-recommendation-title');
+
     expect(title).toBeInTheDocument();
+    expect(img).toBeInTheDocument();
+    expect(category).toBeInTheDocument();
+    expect(instructions).toBeInTheDocument();
+    expect(video).toBeInTheDocument();
+    expect(recomendation).toBeInTheDocument();
+    expect(recomendationTitle).toBeInTheDocument();
   });
 });
