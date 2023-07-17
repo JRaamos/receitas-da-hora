4f9fcb30d6d704ae85be267f50fe7c234531d4a8
JRaamos <jhonyramos46@gmail.com>
2023-07-17 02:19:00 -0300

    feat: adiciona pagina de receitas prontas

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
+import TelaDoneRecipes from '../components/TelaDoneRecipes';
 
 function DoneRecipes() {
   return (
     <div>
-      <Header />
+      <header>
+        <Header />
+      </header>
+      <main>
+        <TelaDoneRecipes />
+      </main>
     </div>
   );
 }
