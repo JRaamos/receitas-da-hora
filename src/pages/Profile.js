af5739a5b68fbdd69257c06a2b64688ce4a3a629
JRaamos <jhonyramos46@gmail.com>
2023-07-18 00:17:00 -0300

    feat: adiciona email do usuario a tela

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,13 +2,15 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const email = JSON.parse(localStorage.getItem('user'));
+  console.log(email);
   return (
     <div>
       <header>
         <Header />
       </header>
       <main>
-        <p data-testid="profile-email">email</p>
+        <p data-testid="profile-email">{email.email}</p>
         <button
           type="button"
           data-testid="profile-done-btn"
