4a8c66b79fabe63c3b3bf66d90172835f9b0e40a
JRaamos <jhonyramos46@gmail.com>
2023-07-19 04:03:01 -0300

    feat: criar logica para os botões de compartilhar e favoritar da tela de receitas em progresso

--- src/components/RecipeInProgres.js
+++ src/components/RecipeInProgres.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 import './recipeInProgres.css';
+import clipboardCopy from 'clipboard-copy';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 import { fetchApiDrikId, fetchApiMealsId } from '../helpers/fetchApi';
 
@@ -8,10 +12,21 @@ function RecipeInProgress() {
   const [item, setItem] = useState([]);
   const [ingredients, setIngredients] = useState([]);
   const [allIngredients, setallIngredients] = useState([]);
+  const [favorite, setFavorite] = useState(false);
+  const [copyLink, setCopyLink] = useState(false);
   const location = useLocation();
   const { pathname } = location;
+  const copy = clipboardCopy;
+  const favoriteIcon = () => {
+    const id = pathname.split('/')[2];
+    const favoritList = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    setFavorite(favoritList.some((e) => e.id === id));
+  };
 
   useEffect(() => {
+    if (!localStorage.getItem('favoriteRecipes')) {
+      localStorage.setItem('favoriteRecipes', JSON.stringify([]));
+    }
     const recipeDetaisl = async () => {
       const type = pathname.split('/')[1];
       const id = pathname.split('/')[2];
@@ -24,6 +39,7 @@ function RecipeInProgress() {
       }
     };
     recipeDetaisl();
+    favoriteIcon();
   }, []);
 
   useEffect(() => {
@@ -37,7 +53,7 @@ function RecipeInProgress() {
     setIngredients(ingredientFilter);
   }, [item]);
 
-  const handleCheckboxChange = (index) => {
+  const handleCheckbox = (index) => {
     const type = pathname.split('/')[1];
     const id = pathname.split('/')[2];
     const newIngredientes = [...allIngredients];
@@ -71,25 +87,83 @@ function RecipeInProgress() {
       }
     }
   }, [pathname]);
+
+  const handleShare = () => {
+    const type = pathname.split('/')[1];
+    const id = pathname.split('/')[2];
+    copy(`http://localhost:3000/${type}/${id}`);
+    setCopyLink(true);
+  };
+  const handleFavorite = () => {
+    const favoritList = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const type = pathname.split('/')[1];
+    if (!favorite) {
+      if (type === 'meals') {
+        const favoritarMeals = {
+          id: item.idMeal,
+          type: 'meal',
+          nationality: item.strArea,
+          category: item.strCategory,
+          alcoholicOrNot: '',
+          name: item.strMeal,
+          image: item.strMealThumb,
+        };
+        localStorage.setItem(
+          'favoriteRecipes',
+          JSON.stringify([...favoritList, favoritarMeals]),
+        );
+      }
+      if (type === 'drinks') {
+        const favoritarDrinks = {
+          id: item.idDrink,
+          type: 'drink',
+          category: item.strCategory,
+          alcoholicOrNot: item.strAlcoholic,
+          name: item.strDrink,
+          nationality: '',
+          image: item.strDrinkThumb,
+        };
+        localStorage.setItem(
+          'favoriteRecipes',
+          JSON.stringify([...favoritList, favoritarDrinks]),
+        );
+      }
+      setFavorite(true);
+    } else {
+      const newList = favoritList.filter((e) => e.id !== item.idMeal
+        && e.id !== item.idDrink);
+      localStorage.setItem('favoriteRecipes', JSON.stringify(newList));
+      setFavorite(false);
+    }
+  };
+
   return (
     <div>
       <h1 data-testid="recipe-title">titulo</h1>
       <img data-testid="recipe-photo" src="" alt="" />
       <h3 data-testid="recipe-category">category</h3>
+      {
+        copyLink && <p>Link copied!</p>
+      }
       <button
         type="button"
-        data-testid="favorite-btn"
-        // onClick={favoriteButton}
-        // src={ favorite ? blackHeartIcon : whiteHeartIcon }
+        onClick={ handleFavorite }
       >
-        Favorite
+        <img
+          src={ favorite ? blackHeartIcon : whiteHeartIcon }
+          alt="favorite"
+          data-testid="favorite-btn"
+        />
       </button>
       <button
         type="button"
-        data-testid="share-btn"
-        // onClick={ shareButton }
+        onClick={ handleShare }
       >
-        Share
+        <img
+          data-testid="share-btn"
+          src={ shareIcon }
+          alt="share"
+        />
       </button>
       <ul className="ingredient-progress">
         {
@@ -102,7 +176,7 @@ function RecipeInProgress() {
                 <input
                   type="checkbox"
                   id={ index }
-                  onChange={ () => handleCheckboxChange(index) }
+                  onChange={ () => handleCheckbox(index) }
                   checked={ allIngredients.includes(index) }
                 />
                 { ingredient }
