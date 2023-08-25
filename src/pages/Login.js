64817d2f8d9e3ba06abf25033317d84101e6fbec
JRaamos <jhonyramos46@gmail.com>
2023-08-25 18:46:55 -0300

    feat: adiciona validação de email e de senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './Login.css';
 import { useDispatch } from 'react-redux';
@@ -8,27 +8,38 @@ import { handleGravatarEmail } from '../redux/actions';
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isDisabled, setIsDisabled] = useState(true);
+  const [isMessagemEmail, setIsMessagemEmail] = useState(false);
+  const [isMessagemPassword, setIsMessagemPassword] = useState(false);
   const history = useHistory();
   const dispatch = useDispatch();
-  useEffect(() => {
-    // função responsavel por validar o email e a senha
-    const handleValidation = () => {
-      const MIN_PASSWORD_LENGTH = 6;
-      const emailValidation = /.+@[A-z]+[.]com/;
-      if (emailValidation.test(email) && password.length > MIN_PASSWORD_LENGTH) {
-        setIsDisabled(false);
-      } else {
-        setIsDisabled(true);
-      }
-    };
-    handleValidation();
-  }, [email, password]);
-  // função responsavel por salvar o email no localStorage e redirecionar para a pagina de comidas
-  const handleSubmit = () => {
-    localStorage.setItem('user', JSON.stringify({ email }));
-    history.push('/meals');
+
+  // função responsavel por validar o email e a senha
+  const handleValidation = () => {
+    const MIN_PASSWORD_LENGTH = 6;
+    const emailValidation = /.+@[A-z]+[.]com/;
+    if (!emailValidation.test(email)) {
+      setIsMessagemEmail(true);
+    } else {
+      setIsMessagemEmail(false);
+    }
+    if (password.length < MIN_PASSWORD_LENGTH) {
+      setIsMessagemPassword(true);
+    } else {
+      setIsMessagemPassword(false);
+    }
   };
+  const handleValidationAndSubmit = () => {
+    const MIN_PASSWORD_LENGTH = 6;
+    const emailValidation = /.+@[A-z]+[.]com/;
+    if (emailValidation.test(email) && password.length > MIN_PASSWORD_LENGTH) {
+      dispatch(handleGravatarEmail(handleGravatar(email)));
+      setIsMessagemEmail(false);
+      setIsMessagemPassword(false);
+      localStorage.setItem('user', JSON.stringify({ email }));
+      history.push('/meals');
+    }
+  };
+
   return (
     <div className="login-contain">
       <div>
@@ -37,6 +48,16 @@ function Login() {
           alt="comida"
           className="login-img"
         />
+      </div>
+      <div className="login-messagem">
+        {
+          isMessagemEmail
+          && <p>O email deve ser no formato `exemplo@exem.com`</p>
+        }
+        {
+          isMessagemPassword
+           && <p> A senha deve ter no minimo 6 caracteres</p>
+        }
       </div>
       <form className="forms-login">
         <h1 className="login-title">Login</h1>
@@ -66,17 +87,16 @@ function Login() {
           className="login-btn"
           type="button"
           data-testid="login-submit-btn"
-          disabled={ isDisabled }
           onClick={ () => {
-            handleSubmit();
-            dispatch(handleGravatarEmail(handleGravatar(email)));
+            handleValidation();
+            handleValidationAndSubmit();
           } }
         >
           Entrar
         </button>
         <button
           className="login-btn"
-          onClick={ handleSubmit }
+          onClick={ () => history.push('/meals') }
         >
           Continuar sem login
         </button>
